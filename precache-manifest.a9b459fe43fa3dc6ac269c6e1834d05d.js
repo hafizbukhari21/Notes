938a92ffcd349f9ae06b7c66fedc734861e07780
M. Hafiz Bukhari <hafiz.bukhari@hotmail.com>
2020-07-22 19:25:49 +0700

    Updates

---
+++ precache-manifest.a9b459fe43fa3dc6ac269c6e1834d05d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90f03d2ad2bf7e1a09c0887985da46e",
+    "url": "/Notes/index.html"
+  },
+  {
+    "revision": "f8e5be8abf2a16a02c44",
+    "url": "/Notes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f57cc0003a6685ddf5e9",
+    "url": "/Notes/static/js/2.4a1813ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Notes/static/js/2.4a1813ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e5be8abf2a16a02c44",
+    "url": "/Notes/static/js/main.396b7fd3.chunk.js"
+  },
+  {
+    "revision": "78d0032a64d2b723ac05",
+    "url": "/Notes/static/js/runtime-main.6395584d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Notes/static/media/logo.5d5d9eef.svg"
+  }
+]);
