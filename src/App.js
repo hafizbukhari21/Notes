0976f605b8b12141042444256cdbab0b9bb00d14
M. Hafiz Bukhari <hafiz.bukhari@hotmail.com>
2020-07-22 19:43:42 +0700

    asdasd

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and sa dasdasdas asd asd asd as asd asd asd asd asd asd ve to reload.
+          Edit <code>src/App.js</code> and sa dasdasdas asd asd asd as asd asd asd ohasd cvcvcvc x x x x x x xve to reload.
         </p>
         <a
           className="App-link"
